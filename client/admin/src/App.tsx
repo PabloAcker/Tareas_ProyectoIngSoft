f95ab651e105c5a67aed5989e99c958272a99220
Priset <emanuel.1kll@gmail.com>
2023-09-12 12:44:57 -0400

    Tuqui

--- client/admin/src/App.tsx
+++ client/admin/src/App.tsx
@@ -1,12 +1,11 @@
 import "./App.css";
-import Component from "./components/TitleCard";
-import { Component } from "react";
+import LoginForm from "./components/Component";
+
 
 function App() {
   return (
     <>
-      <Component name="Paul Landaeta"/>
-      <Component name="Emanuel Lozano"/>
+      <LoginForm/>
     </>
   );
 }
--- client/admin/src/components/Component.tsx
+++ client/admin/src/components/Component.tsx
@@ -1,1 +1,56 @@
+import React, { useState } from 'react';
 
+const LoginForm: React.FC = () => {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
+  return (
+    <div className="max-w-md mx-auto mt-4 p-6 bg-white rounded-lg shadow-md">
+      <h2 className="text-2xl font-semibold mb-4">Ingresar</h2>
+
+      <form>
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-gray-600">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-400"
+            placeholder="Tu Email"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="password" className="block text-gray-600">Password</label>
+          <div className="relative">
+            <input
+              type={passwordVisible ? "text" : "password"}
+              id="password"
+              name="password"
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-400"
+              placeholder="Tu Contraseña"
+            />
+            <span
+              className="absolute top-2 right-3 text-blue-600 cursor-pointer"
+              onClick={togglePasswordVisibility}
+            >
+              {passwordVisible ? "Ocultar" : "Mostrar"}
+            </span>
+          </div>
+        </div>
+
+        <button
+          type="submit"
+          className="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg focus:outline-none"
+        >
+          Ingresar
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
